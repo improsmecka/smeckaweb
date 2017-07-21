1917d22bd7e2b703c80f9edeaa359ba09b01b03c
Improsmečka <improsmecka@gmail.com>
2017-07-21 22:18:26 +0200

    User publicname

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -25,6 +25,19 @@ class User extends BaseUser
     
      /** @ORM\Column(type="decimal", scale=2, nullable=true, options={ "default":0}) */
     private $ferocity;
+    
+    /** @ORM\Column(type="string",  nullable=true) */
+    private $description;
+    public function getDescription(){
+        return $this->description;            
+    }
+    
+    /** @ORM\Column(type="string",  nullable=true) */
+    private $publicName;
+    public function getPublicName(){
+        return $this->publicName;            
+    }
+    
    
     /** @ORM\OneToMany(targetEntity="Event", mappedBy="user") */
     private $events;
