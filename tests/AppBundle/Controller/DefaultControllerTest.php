1c4298e64fa79acaab7e6ce48b208db03c884b1e
Improsmečka <improsmecka@gmail.com>
2017-07-29 11:23:48 +0200

    Dev/prod/test  settings

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -13,6 +13,7 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Improsmečka', $crawler->filter('.container h1')->text());
+        $this->assertContains('Uživatel', $crawler->filter('.container h2')->text());
     }
 }
