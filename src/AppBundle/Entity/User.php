ece795431003f331949905a5b98b69b24ab5d574
Improsmečka <improsmecka@gmail.com>
2017-07-30 11:48:39 +0200

    Add fields to registration

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,11 +1,11 @@
 <?php
 
-
 namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection	as ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -26,17 +26,38 @@ class User extends BaseUser
      /** @ORM\Column(type="decimal", scale=2, nullable=true, options={ "default":0}) */
     private $ferocity;
     
-    /** @ORM\Column(type="string",  nullable=true) */
+    /** @ORM\Column(type="string",  nullable=true) */    
     private $description;
     public function getDescription(){
         return $this->description;            
     }
+    public function setDescription($Description){
+        $this->description=$Description;        
+    }
     
-    /** @ORM\Column(type="string",  nullable=true) */
+   
+     /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Please enter your public name.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="Public name is too short.",
+     *     maxMessage="Public name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+   
     private $publicName;
     public function getPublicName(){
         return $this->publicName;            
     }
+    public function setPublicName($publicName){
+        $this->publicName=$publicName;        
+    }
+    
+    
     
    
     /** @ORM\OneToMany(targetEntity="Event", mappedBy="user") */
